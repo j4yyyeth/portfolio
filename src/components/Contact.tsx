d25820b8f4311853f3d5f8a06d3a55db5e23606b
Jacob Handwerger <jstudyeth@gmail.com>
2023-09-14 17:03:11 -0400

    form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,34 +17,53 @@ export default function Contact() {
   };
   return (
     <form id="contact-form" ref={form} onSubmit={handleSubmit}>
-      <div className="input-container">
-        <input type="text" id="input" name="name" required></input>
-        <label htmlFor="input" className="label">
-          Name
-        </label>
-        <div className="underline"></div>
+      {sent ? (
+        <></>
+      ) : (
+        <>
+          <div className="input-container">
+            <input type="text" id="input" name="name" required></input>
+            <label htmlFor="input" className="label">
+              Name
+            </label>
+            <div className="underline"></div>
+          </div>
+          <div className="input-container">
+            <input type="text" id="input" name="email" required></input>
+            <label htmlFor="input" className="label">
+              Email
+            </label>
+            <div className="underline"></div>
+          </div>
+          <div className="input-container">
+            <input type="text" id="input" name="message" required></input>
+            <label htmlFor="input" className="label">
+              Message
+            </label>
+            <div className="underline"></div>
+          </div>
+        </>
+      )}
+      <div className="centered-submit">
+        {sent ? (
+          <button className="form-btn-disabled" disabled>
+            <span>Thank You</span>
+          </button>
+        ) : (
+          <button type="submit" className="form-btn" value="send">
+            <span>Send</span>
+            <svg viewBox="0 0 13 10" height="10px" width="15px">
+              <path d="M1,5 L11,5"></path>
+              <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+          </button>
+        )}
+        {sent ? (
+          <div className="typing-demo">I'll get back to you soon!</div>
+        ) : (
+          <></>
+        )}
       </div>
-      <div className="input-container">
-        <input type="text" id="input" name="email" required></input>
-        <label htmlFor="input" className="label">
-          Email
-        </label>
-        <div className="underline"></div>
-      </div>
-      <div className="input-container">
-        <input type="text" id="input" name="message" required></input>
-        <label htmlFor="input" className="label">
-          Message
-        </label>
-        <div className="underline"></div>
-      </div>
-      <button type="submit" className="form-btn" value="send">
-        <span>Send</span>
-        <svg viewBox="0 0 13 10" height="10px" width="15px">
-          <path d="M1,5 L11,5"></path>
-          <polyline points="8 1 12 5 8 9"></polyline>
-        </svg>
-      </button>
     </form>
   );
 }
