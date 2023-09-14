6859c444c88b0fe6d4cebb44c0fcdd8afd1278e5
Jacob Handwerger <jstudyeth@gmail.com>
2023-09-14 16:42:25 -0400

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,28 +1,44 @@
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
+  const [sent, setSent] = useState(false);
+  const form = useRef();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      import.meta.env.PUBLIC_EMAIL_JS_SERVICE_ID,
+      import.meta.env.PUBLIC_EMAIL_JS_TEMPLATE_ID,
+      form.current,
+      import.meta.env.PUBLIC_EMAIL_JS_PUBLIC_ID
+    );
+    setSent(true);
+    e.target.reset();
+  };
   return (
-    <form id="contact-form">
+    <form id="contact-form" ref={form} onSubmit={handleSubmit}>
       <div className="input-container">
-        <input type="text" id="input" required></input>
+        <input type="text" id="input" name="name" required></input>
         <label htmlFor="input" className="label">
           Name
         </label>
         <div className="underline"></div>
       </div>
       <div className="input-container">
-        <input type="text" id="input" required></input>
+        <input type="text" id="input" name="email" required></input>
         <label htmlFor="input" className="label">
           Email
         </label>
         <div className="underline"></div>
       </div>
       <div className="input-container">
-        <input type="text" id="input" required></input>
+        <input type="text" id="input" name="message" required></input>
         <label htmlFor="input" className="label">
           Message
         </label>
         <div className="underline"></div>
       </div>
-      <button type="submit" className="form-btn">
+      <button type="submit" className="form-btn" value="send">
         <span>Send</span>
         <svg viewBox="0 0 13 10" height="10px" width="15px">
           <path d="M1,5 L11,5"></path>
